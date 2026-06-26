54125c04d79e2a92be477c79300a18cd97ba11e5
Andżelika <andzelika@MacBook-Pro.local>
2026-06-26 10:22:12 +0200

    deploy

--- resources/views/blocks/reviews.blade.php
+++ resources/views/blocks/reviews.blade.php
@@ -3,28 +3,17 @@
 <section
 	data-gsap-anim="section"
 	@if(!empty($section_id)) id="{{ $section_id }}" @endif
-<<<<<<< Updated upstream
 	@class([ 'b-reviews relative overflow-hidden -spt -spb bg-[#091838]' ,
-=======
-	@class([ 'b-reviews relative overflow-hidden -smt -spt -spb bg-[#091838]' ,
->>>>>>> Stashed changes
 	$sectionClass=> filled($sectionClass),
 	$section_class => filled($section_class),
 	$background => filled($background) && $background !== 'none',
 	])>
 	<div class="blur bg-primary-100"></div>
 	<div class="second_blur bg-primary-100"></div>
-<<<<<<< Updated upstream
 	<div class="absolute right_shape z-12 hidden md:block"><img class="" src="{{ get_template_directory_uri() }}/resources/images/shape_s.svg"></div>
 
 	<div class="absolute gradient-left inset-y-0 left-0 md:w-[24vw] w-[14vw] z-10" style="background: linear-gradient(90deg, #091838 0%, rgba(9, 24, 56, 0.00) 100%);"></div>
 	<div class="absolute gradient-right inset-y-0 right-0 md:w-[24vw] w-[14vw] z-10" style="background: linear-gradient(90deg, rgba(9, 24, 56, 0.00) 0%, #091838 100%);"></div>
-=======
-	<div class="absolute right_shape z-12"><img class="" src="{{ get_template_directory_uri() }}/resources/images/shape_s.svg"></div>
-
-	<div class="absolute gradient-left inset-y-0 left-0 w-[60vw] z-10" style="background: linear-gradient(90deg, #091838 0%, rgba(9, 24, 56, 0.00) 100%);"></div>
-	<div class="absolute gradient-right inset-y-0 right-0 w-[30vw] z-10" style="background: linear-gradient(90deg, rgba(9, 24, 56, 0.00) 0%, #091838 100%);"></div>
->>>>>>> Stashed changes
 
 	<div class="__wrapper c-main">
 		<div class="__content">
@@ -35,7 +24,6 @@
 				<div data-gsap-element="swiper" class="swiper reviews-swiper overflow-visible!">
 					<div class="swiper-wrapper flex">
 						@foreach($r_reviews as $card)
-<<<<<<< Updated upstream
 						<div class="swiper-slide w-full! md:w-[calc(100%/1.5)]! max-w-[calc(100%/1.5)]! md:px-14!">
 						
 
@@ -46,60 +34,10 @@
 										<img class="mix-blend-overlay" src="{{ get_template_directory_uri() }}/resources/images/quote.svg" />
 										<div class="__txt text-h4 -mt-10 mb-9 font-normal!">{!! $card['txt'] !!}</div>
 									</div>
-									@endif
 
 									<b class="font-header text-xl text-white! font-semibold!">{{ $card['name'] }}</b>
 									<b class="font-header text-xl text-white!">{{ $card['text'] }}</b>
 
-=======
-						<div class="swiper-slide w-[calc(100%/1.5)]! max-w-[calc(100%/1.5)]! px-4!">
-							<div class="__card relative px-8 py-14">
-
-								<div class="relative z-10 flex flex-col ">
-									@if(!empty($card['txt']))
-									<div class="review-content-wrapper text-white">
-
-										<img class="mix-blend-overlay" src="{{ Vite::asset('resources/images/quote.svg') }}" />
-										<div class="__txt line-clamp-6 text-h4 -mt-10 mb-9 font-normal!">{!! $card['txt'] !!}</div>
-									</div>
-									@endif
-
-									<b class="font-header text-xl text-white! font-semibold!">{{ $card['name'] }}</b>
-									<b class="font-header text-xl text-white!">{{ $card['text'] }}</b>
-
->>>>>>> Stashed changes
-								</div>
-							</div>
-						</div>
-						@endforeach
-					</div>
-<<<<<<< Updated upstream
-				</div>
-				<div data-gsap-element="arrows" class="absolute top-1/2 left-0 w-full -translate-y-1/2 z-20 flex justify-between items-center pointer-events-none z-12">
-					<div class="__prev  flex items-center justify-center pointer-events-auto -translate-x-1/2 cursor-pointer transition-all duration-400 ">
-						<x-icon.arrow-left class="cursor-pointer h-10 w-auto" />
-					</div>
-					<div class="__next  flex items-center justify-center pointer-events-auto translate-x-1/2 cursor-pointer transition-all duration-300 ">
-						<x-icon.arrow-right class="cursor-pointer h-10 w-auto" />
-=======
-
-					<!-- <div data-gsap-element="arrows" class="absolute top-1/2 left-0 w-full -translate-y-1/2 z-20 flex justify-between items-center pointer-events-none ">
-					<div class="__prev  flex items-center justify-center pointer-events-auto -translate-x-1/2 cursor-pointer transition-all duration-400 z-20">
-						<x-icon.arrow-left class="cursor-pointer h-10 w-auto z-20" />
-					</div>
-					<div class="__next  flex items-center justify-center pointer-events-auto translate-x-1/2 cursor-pointer transition-all duration-300 z-20 ">
-						<x-icon.arrow-right class="cursor-pointer h-10 w-auto z-20" />
-					</div>
-				</div> -->
-
-				</div>
-				<div data-gsap-element="arrows" class="absolute top-1/2 left-0 w-full -translate-y-1/2 z-20 flex justify-between items-center pointer-events-none ">
-					<div class="__prev  flex items-center justify-center pointer-events-auto -translate-x-1/2 cursor-pointer transition-all duration-400 z-20">
-						<x-icon.arrow-left class="cursor-pointer h-10 w-auto z-20" />
-					</div>
-					<div class="__next  flex items-center justify-center pointer-events-auto translate-x-1/2 cursor-pointer transition-all duration-300 z-20 ">
-						<x-icon.arrow-right class="cursor-pointer h-10 w-auto z-20" />
->>>>>>> Stashed changes
 					</div>
 				</div>
 			</div>
--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -7,7 +7,6 @@ $social_media = get_field('social_media', 'option');
 
 <header x-data="{ mobileOpen: false }" class="relative top-0 z-50 bg-background masthead fixed-top">
 
-<<<<<<< Updated upstream
     <!-- Desktop Header -->
     <div class="items-center justify-between hidden h-full py-4 px-12 mx-auto lg:flex">
         <a class="brand shrink-0" href="{{ home_url('/') }}">
@@ -28,28 +27,6 @@ $social_media = get_field('social_media', 'option');
             ]) !!}
         </nav>
         @endif
-=======
-	<!-- Desktop Header -->
-	<div class="items-center justify-between hidden h-full py-4 px-12 mx-auto md:flex">
-		<a class="brand shrink-0" href="{{ home_url('/') }}">
-			@if ($logo)
-			<img src="{{ $logo['url'] }}" alt="{{ $logo['alt'] ?? 'Logo' }}" class="w-auto h-14">
-			@else
-			<span class="text-xl font-bold">{{ $siteName }}</span>
-			@endif
-		</a>
-		@if (has_nav_menu('primary_navigation'))
-		<nav class="ml-6 lg:ml-15 nav-primary w-full" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-			{!! wp_nav_menu([
-			'theme_location' => 'primary_navigation',
-			'menu_class' => 'nav flex gap-x-3 lg:gap-x-6 text-lg font-medium justify-center items-center',
-			'container' => false,
-			'echo' => false,
-			'walker' => new DropdownWalker(),
-			]) !!}
-		</nav>
-		@endif
->>>>>>> Stashed changes
 
         <div class="flex items-center gap-x-4 shrink-0">
             <a href="/kontakt/" class="btn btn-secondary">
@@ -61,7 +38,6 @@ $social_media = get_field('social_media', 'option');
         </div>
     </div>
 
-<<<<<<< Updated upstream
     <!-- Mobile Header Bar -->
     <div class="flex items-center justify-between p-4 mobile-menu fixed-top lg:hidden">
         <a class="brand shrink-0" href="{{ home_url('/') }}">
@@ -85,43 +61,6 @@ $social_media = get_field('social_media', 'option');
             </svg>
         </button>
     </div>
-=======
-		<div class="flex items-center gap-x-4 shrink-0">
-    <a href="/kontakt/" class="btn btn-secondary">
-        Kontakt
-    </a>
-
-    @if(!empty($social_media))
-        <div class="flex items-center gap-x-3 text-white">
-            @foreach($social_media as $social)
-                @if(!empty($social['link']))
-                    <a href="{{ $social['link']['url'] }}" 
-                       target="{{ $social['link']['target'] ?? '_blank' }}" 
-                       class="hover:text-secondary-100 transition-colors duration-200"
-                       title="{{ $social['link']['title'] ?? $social['icon'] }}">
-                        
-                        @if($social['icon'] === 'facebook')
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="24" viewBox="0 0 13 24" fill="none">
-  <path d="M3.57972 13.5469C3.49022 13.5469 1.52138 13.5469 0.62645 13.5469C0.149155 13.5469 0 13.3667 0 12.9161C0 11.7146 0 10.4831 0 9.2816C0 8.801 0.178986 8.65081 0.62645 8.65081H3.57972C3.57972 8.5607 3.57972 6.81852 3.57972 6.00751C3.57972 4.80601 3.78853 3.66458 4.38515 2.61327C5.0116 1.53191 5.90653 0.811013 7.04011 0.390488C7.78588 0.12015 8.53165 0 9.33709 0H12.2605C12.6782 0 12.8571 0.180225 12.8571 0.600751V4.02503C12.8571 4.44556 12.6782 4.62578 12.2605 4.62578C11.4551 4.62578 10.6497 4.62578 9.84422 4.65582C9.03878 4.65582 8.62115 5.04631 8.62115 5.88736C8.59132 6.78849 8.62115 7.65957 8.62115 8.59074H12.0815C12.5588 8.59074 12.7378 8.77096 12.7378 9.25156V12.8861C12.7378 13.3667 12.5887 13.5169 12.0815 13.5169C11.0076 13.5169 8.71064 13.5169 8.62115 13.5169V23.3091C8.62115 23.8198 8.47199 24 7.93504 24C6.68214 24 5.45907 24 4.20617 24C3.7587 24 3.57972 23.8198 3.57972 23.3692C3.57972 20.2153 3.57972 13.637 3.57972 13.5469Z" fill="white"/>
-</svg>
-                            
-                        @elseif($social['icon'] === 'instagram')
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-<path d="M12.0042 5.83728C8.60123 5.83728 5.84623 8.59524 5.84623 11.9952C5.84623 15.3981 8.60423 18.1531 12.0042 18.1531C15.4072 18.1531 18.1622 15.3951 18.1622 11.9952C18.1622 8.59224 15.4042 5.83728 12.0042 5.83728ZM12.0042 15.9921C9.79523 15.9921 8.00723 14.2031 8.00723 11.9952C8.00723 9.78722 9.79623 7.99825 12.0042 7.99825C14.2122 7.99825 16.0012 9.78722 16.0012 11.9952C16.0022 14.2031 14.2132 15.9921 12.0042 15.9921Z" fill="white"/>
-<path d="M16.9482 0.0753853C14.7402 -0.0276129 9.27123 -0.022613 7.06123 0.0753853C5.11923 0.166384 3.40623 0.635375 2.02523 2.01635C-0.282773 4.32431 0.0122273 7.43426 0.0122273 11.9952C0.0122273 16.6631 -0.247773 19.701 2.02523 21.974C4.34223 24.29 7.49723 23.987 12.0042 23.987C16.6282 23.987 18.2242 23.99 19.8592 23.357C22.0822 22.494 23.7602 20.507 23.9242 16.9381C24.0282 14.7291 24.0222 9.26123 23.9242 7.05126C23.7262 2.83834 21.4652 0.283382 16.9482 0.0753853ZM20.4432 20.447C18.9302 21.96 16.8312 21.825 11.9752 21.825C6.97523 21.825 4.97023 21.899 3.50723 20.432C1.82223 18.7551 2.12723 16.0621 2.12723 11.9792C2.12723 6.45427 1.56023 2.47534 7.10523 2.19135C8.37923 2.14635 8.75423 2.13135 11.9612 2.13135L12.0062 2.16135C17.3352 2.16135 21.5162 1.60336 21.7672 7.14726C21.8242 8.41224 21.8372 8.79223 21.8372 11.9942C21.8362 16.9361 21.9302 18.9531 20.4432 20.447Z" fill="white"/>
-<path d="M18.4062 7.03326C19.201 7.03326 19.8452 6.38901 19.8452 5.59429C19.8452 4.79956 19.201 4.15531 18.4062 4.15531C17.6115 4.15531 16.9672 4.79956 16.9672 5.59429C16.9672 6.38901 17.6115 7.03326 18.4062 7.03326Z" fill="white"/>
-</svg>
-                        @endif
-
-                    </a>
-                @endif
-            @endforeach
-        </div>
-    @endif
-</div>
-	</div>
->>>>>>> Stashed changes
 
     <!-- Mobile Menu Panel -->
     <div
@@ -169,16 +108,5 @@ $social_media = get_field('social_media', 'option');
             </div>
         </div>
 
-<<<<<<< Updated upstream
     </div>
-=======
-			<div class="mt-8">
-				<a href="/kontakt/" class="block w-full btn btn-secondary ">
-					Kontakt
-				</a>
-			</div>
-		</div>
-
-	</div>
->>>>>>> Stashed changes
 </header>
